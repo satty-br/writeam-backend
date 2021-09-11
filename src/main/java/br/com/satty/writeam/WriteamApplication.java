8c4479e03f8acb23d6c67965137160fbdbc4f07d
Ricardo Zandonai <squall@outlook.com>
2021-09-11 11:14:58 -0300

    Init commit

---
+++ src/main/java/br/com/satty/writeam/WriteamApplication.java
@@ -0,0 +1,13 @@
+package br.com.satty.writeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/satty/writeam/WriteamApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.satty.writeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
